e218488f9e494e4709eba7eda1588658cf73be84
PavelOsw <pavel.olage@alumnos.udg.mx>
2022-10-18 23:04:06 -0500

    Listas de repro., pagos y facturas

---
+++ Facturas.cpp
@@ -0,0 +1,457 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <windows.h>
+
+
+using namespace std;
+int orden=0;
+
+class facturaCliente
+{
+    public:
+    char IdCliente[10], nombreCliente[100];
+    char dirCliente[100],coloniaCliente[100];
+    char ciudadCliente[100],codigoPostal[100];
+    float pagoAlvel;
+
+    void agregar();
+    void imprimir();
+    void buscar();
+    void modificar();
+    void eliminar();
+    void Mostrar_Indice();
+    void Orden();
+
+}factura;
+
+class indiceInvertido
+{
+    public:
+    char IdCliente1[10];
+    long int pos;
+
+}indiceInvert;
+
+void facturaCliente::imprimir()
+{
+    ifstream a("datos.bin");
+    if(!a.good())
+    cout<<"UPS! No existe el archivo";
+    else
+    {
+        while(!a.eof())
+        {
+            a.read((char*)&factura,sizeof(factura));
+            if(a.eof())
+                break;
+            cout<<"ID del Cliente: "<<factura.IdCliente<<endl<<"Nombre del cliente: "<<factura.nombreCliente<<endl<<"Direccion del Cliente: "<<factura.dirCliente<<endl<<"Colonia del Cliente: "<<factura.coloniaCliente<<endl<<"Ciudad: "<<factura.ciudadCliente<<endl<<"Codigo Postal: "<<codigoPostal<<endl<<"Pago: "<<pagoAlvel<<endl;
+        }
+    }
+    a.close();
+}
+
+void facturaCliente::Mostrar_Indice()
+{
+  ifstream a("indice.bin");
+    if(!a.good())
+    cout<<"UPS! No existe el archivo";
+    else
+    {
+        while(!a.eof())
+        {
+            a.read((char*)&indiceInvert,sizeof(indiceInvert));
+            if(a.eof())
+                break;
+            cout<<"ID del Cliente: "<<indiceInvert.IdCliente1<<endl<<endl;
+        }
+    }
+    a.close();
+}
+
+
+
+
+void facturaCliente::agregar()
+{
+    fflush(stdin);
+    cout<<"\nID del Cliente: ";
+    cin.getline(IdCliente,10);
+    cout<<"\nNombre del cliente: ";
+    cin.getline(nombreCliente,100);
+    cout<<"\nDireccion: ";
+    cin.getline(dirCliente,100);
+    cout<<"\nColonia: ";
+    cin.getline(coloniaCliente,100);
+    cout<<"\nCiudad: ";
+    cin.getline(ciudadCliente,100);
+    fflush(stdin);
+    cout<<"\nCodigo Postal: ";
+    cin.getline(codigoPostal,100);
+    cout<<"\nPago Alvel (Ingresa Cantidad en pesos $): ";
+    cin>>pagoAlvel;
+
+
+
+
+    ofstream a("datos.bin",ios::app);
+    a.write((char*)&factura,sizeof(factura));
+    indiceInvert.pos=a.tellp();
+    indiceInvert.pos-=sizeof(factura);
+    a.close();
+    strcpy(indiceInvert.IdCliente1,IdCliente);
+    ofstream b("indice.bin",ios::app);
+    b.write((char*)&indiceInvert,sizeof(indiceInvert));
+    b.close();
+    orden++;
+
+    cout<<"=======================================================\n";
+    cout<<"SE AGREGO A LA LISTA DE REPDORUCCION CORRECTAMENTE!!\n";
+    cout<<"=======================================================\n";
+}
+
+void facturaCliente::buscar()
+{
+    char idbus[14];
+    int band=0;
+    //cout<<"posicion "<<y.pos;
+    ifstream b("indice.bin");
+    if(!b.good()){
+    cout<<"UPS! No existe el archivo";}
+    else{
+    cout<<"Ingresa el ID del cliente para buscar los datos\n";
+    cout<<"\nID Cliente: "<<endl;
+    fflush(stdin);cin.getline(idbus,10);
+    while(!b.eof())
+    {
+        b.read((char*)&indiceInvert,sizeof(indiceInvert));
+        if(b.eof())
+        {
+            break;
+        }
+        if(strcmp(indiceInvert.IdCliente1,idbus)==0)
+        {
+            ifstream a("datos.bin");
+            a.seekg(indiceInvert.pos,ios::beg);
+            a.read((char*)&factura,sizeof(factura));
+            cout<<"ID del Cliente: "<<factura.IdCliente<<endl<<"Nombre del cliente: "<<factura.nombreCliente<<endl<<"Direccion del Cliente: "<<factura.dirCliente<<endl<<"Colonia del Cliente: "<<factura.coloniaCliente<<endl<<"Ciudad: "<<factura.ciudadCliente<<endl<<"Codigo Postal: "<<codigoPostal<<endl<<"Pago: "<<pagoAlvel<<endl;
+            a.close();
+            band=1;
+            break;
+        }
+    }
+    if(band==0)
+    {
+     cout<<"\n NO EXISTE REGISTRO....."   ;
+    }
+    }
+    b.close();
+
+}
+
+void facturaCliente::modificar()
+{
+     char idmod[15];
+     int band=0;
+     int opc;
+     ifstream b("indice.bin");
+     if(!b.good()){
+     cout<<"No existe el archivo";}
+     else{
+     cout<<"Ingresa el ID del cliente para modificar los datos\n";
+    cout<<"\nID Cliente: "<<endl;
+     fflush(stdin);cin.getline(idmod,10);
+     while(!b.eof())
+    {   opc=0;
+        b.read((char*)&indiceInvert,sizeof(indiceInvert));
+        if(b.eof())
+        {
+            break;
+        }
+        if(strcmp(indiceInvert.IdCliente1,idmod)==0 && band==0)
+        {
+            ifstream a("datos.bin");
+            a.seekg(indiceInvert.pos,ios::beg);
+
+             a.read((char*)&factura,sizeof(factura));
+             if(a.eof())   break;
+            cout<<"ID del Cliente: "<<factura.IdCliente<<endl<<"Nombre del cliente: "<<factura.nombreCliente<<endl<<"Direccion del Cliente: "<<factura.dirCliente<<endl<<"Colonia del Cliente: "<<factura.coloniaCliente<<endl<<"Ciudad: "<<factura.ciudadCliente<<endl<<"Codigo Postal: "<<codigoPostal<<endl<<"Pago: "<<pagoAlvel<<endl;
+            cout<<"Deseas modificarlo?\n"<<"1. [Si]   2. [No]\n"; cin>>opc;
+            band=1;
+            a.close();
+            break;
+         }
+
+         }
+         b.close();
+         if(opc==1)
+{
+    fflush(stdin);
+    ifstream indice("indice.bin");
+    ifstream datos("datos.bin");
+          while(!indice.eof())
+         {
+          indice.read((char*)&indiceInvert,sizeof(indiceInvert));
+          cout<<"\nID: "<<indiceInvert.IdCliente1<<"\n";
+          datos.read((char*)&factura,sizeof(factura));
+           if(indice.eof())
+        {
+            break;
+        }
+        if(strcmp(indiceInvert.IdCliente1,idmod)==0)
+        {
+
+    cout<<"\nID del Cliente: ";
+    cin.getline(IdCliente,10);
+    cout<<"\nNombre del cliente: ";
+    cin.getline(nombreCliente,100);
+    cout<<"\nDireccion: ";
+    cin.getline(dirCliente,100);
+    cout<<"\nColonia: ";
+    cin.getline(coloniaCliente,100);
+    cout<<"\nCiudad: ";
+    cin.getline(ciudadCliente,100);
+    fflush(stdin);
+    cout<<"\nCodigo Postal: ";
+    cin.getline(codigoPostal,100);
+    cout<<"\nPago Alvel (Ingresa Cantidad en pesos $): ";
+    cin>>pagoAlvel;
+
+     }
+
+          ofstream c("temp.bin",ios::app);
+          c.write((char*)&factura,sizeof(factura));
+
+        strcpy(indiceInvert.IdCliente1,IdCliente);
+        ofstream d("tempind.bin",ios::app);
+        d.write((char*)&indiceInvert,sizeof(indiceInvert));
+        d.close();
+        c.close();
+
+        }
+
+
+
+}
+
+    if(band==0)
+    {cout<<"\n NO EXISTE REGISTRO....."   ;}
+
+    }//else
+
+
+    remove("indice.bin"); rename("tempind.bin","indice.bin");
+    remove("datos.bin");  rename("temp.bin","datos.bin");
+
+
+}
+
+void facturaCliente::eliminar()
+{
+ char rfceli[15];
+     int band=0;
+     int opc,cont=0,cont2=0;
+     ifstream b("indice.bin");
+     if(!b.good()){
+     cout<<"UPS! No existe el archivo";}
+     else{
+     cout<<"Ingresa el ID del cliente para eliminar los datos\n";
+     cout<<"\nID Cliente: "<<endl;
+     fflush(stdin);cin.getline(rfceli,10);
+     while(!b.eof())
+    {
+        b.read((char*)&indiceInvert,sizeof(indiceInvert));
+        cont++;
+            cout<<"contador --"<<cont;
+        if(b.eof())
+        {
+            break;
+        }
+        if(strcmp(indiceInvert.IdCliente1,rfceli)==0)
+        {
+            ifstream a("datos.bin");
+            a.seekg(indiceInvert.pos,ios::beg);
+            a.read((char*)&factura,sizeof(factura));
+             if(a.eof())   break;
+
+            cout<<"ID del Cliente: "<<factura.IdCliente<<endl<<"Nombre del cliente: "<<factura.nombreCliente<<endl<<"Direccion del Cliente: "<<factura.dirCliente<<endl<<"Colonia del Cliente: "<<factura.coloniaCliente<<endl<<"Ciudad: "<<factura.ciudadCliente<<endl<<"Codigo Postal: "<<codigoPostal<<endl<<"Pago: "<<pagoAlvel<<endl;
+            cout<<"Deseas modificarlo?\n"<<"1. [Si]   2. [No]\n"; cin>>opc;
+            band=1;
+           a.close();
+             break;
+         }
+
+         }
+        b.close();
+
+
+         if(opc==1)
+         {
+          ifstream indice("indice.bin");
+          ifstream datos("datos.bin");
+          while(!indice.eof())
+         {
+          indice.read((char*)&indiceInvert,sizeof(indiceInvert));
+          cout<<"\nID: "<<indiceInvert.IdCliente1<<"\n";
+          datos.read((char*)&factura,sizeof(factura));
+           if(indice.eof())
+        {
+            break;
+        }
+        if(strcmp(indiceInvert.IdCliente1,rfceli))
+        {cont2++;
+
+
+          ofstream c("temp.bin",ios::app);
+          c.write((char*)&factura,sizeof(factura));
+
+        strcpy(indiceInvert.IdCliente1,IdCliente);
+        ofstream d("tempind.bin",ios::app);
+        d.write((char*)&indiceInvert,sizeof(indiceInvert));
+        d.close();
+        c.close();
+        }
+
+        }
+       indice.close();
+       datos.close();
+       remove("indice.bin"); rename("tempind.bin","indice.bin");
+    remove("datos.bin");  rename("temp.bin","datos.bin");
+        }
+        else
+        {
+
+        }
+
+    if(band==0)
+    {cout<<"\n NO EXISTE REGISTRO....."   ;}
+
+    }
+
+
+}
+
+void facturaCliente::Orden()
+{int i=0;
+char indice[20][14];
+char aux[20];
+
+  ifstream a("indice.bin");
+    if(!a.good())
+    cout<<"UPS! No existe el archivo";
+    else
+    {
+        while(!a.eof())
+        {
+            a.read((char*)&indiceInvert,sizeof(indiceInvert));
+            if(a.eof())
+                break;
+
+                char *cadena=indiceInvert.IdCliente1;
+                strcpy(indice[i],cadena);
+
+                i++;
+        }
+    }
+    a.close();
+
+    for(int i=0;i<orden-1;i++)
+for(int j=i+1;j<orden;j++)
+if(strcmp(indice[i],indice[j])>0)
+{
+strcpy(aux,indice[j]);
+strcpy(indice[j],indice[i]);
+strcpy(indice[i],aux);
+system("cls");}
+for(int i=0;i<orden;i++)
+{
+
+strcpy(indiceInvert.IdCliente1,indice[i]);
+
+ofstream a("tempind.bin",ios::app);
+a.write((char*)&indiceInvert,sizeof(indiceInvert));
+a.close();
+}
+
+ remove("indice.bin");
+ rename("tempind.bin","indice.bin");
+
+}
+
+int main()
+{
+    int opc;
+    do{
+        system("cls");
+        HANDLE hConsole = GetStdHandle( STD_OUTPUT_HANDLE );
+        SetConsoleTextAttribute(hConsole, 15);
+        cout<<"\n==================================================================\n";
+        SetConsoleTextAttribute(hConsole, 3);
+        cout<<"                 --- PELICULAS, SERIES, NOVELAS Y + ---\n\n";
+        SetConsoleTextAttribute(hConsole, 12);
+        cout<<"                               A";
+        SetConsoleTextAttribute(hConsole, 9);
+        cout<<"\n                                L";
+        SetConsoleTextAttribute(hConsole, 6);
+        cout<<"\n                                 V";
+        SetConsoleTextAttribute(hConsole, 14);
+        cout<<"\n                                  E";
+        SetConsoleTextAttribute(hConsole, 10);
+        cout<<"\n                                   L";
+        SetConsoleTextAttribute(hConsole, 13);
+        cout<<"\n                                    +";
+        SetConsoleTextAttribute(hConsole, 3);
+        cout<<"\n\n                 BIENVENIDO AL SERVICIO DE STREAMING!";
+        SetConsoleTextAttribute(hConsole, 15);
+        cout<<"\n==================================================================\n";
+        SetConsoleTextAttribute(hConsole, 7);
+        cout<<"\n\t\t\tMENU PRINCIPAL "<<endl<<endl;
+        SetConsoleTextAttribute(hConsole, 3);
+
+        /*----------------------------------------------------------------------------------------*/
+        cout<<"1) Agregar\n";
+        cout<<"2) Imprimir\n";
+        cout<<"3) Buscar\n";
+        cout<<"4) Modificar\n";
+        cout<<"5) Eliminar\n";
+        cout<<"6) Salir\n";
+        SetConsoleTextAttribute(hConsole, 7);
+        cout<<"\nOPC: ";
+        cin>>opc;
+        switch(opc)
+    {
+
+        case 1:
+        factura.agregar();
+        break;
+
+        case 2:
+        factura.imprimir();
+        break;
+
+        case 3:
+        factura.buscar();
+        break;
+
+        case 4:
+        factura.modificar();
+        break;
+
+        case 5:
+        factura.eliminar();
+        break;
+
+        case 6:
+        cout<<"\nSALIENDO...\n";
+        return 0;
+        break;
+    }
+    getch();
+    }while(opc!=9);
+    return 0;
+}
---
+++ ListaReproduccion.cpp
@@ -0,0 +1,384 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <windows.h>
+
+
+using namespace std;
+int orden=0;
+
+class reproduccionList
+{
+    public:
+    char pelicula[100], nombrePelicula[35], categoria[40],anio[20];
+
+    void agregar();
+    void imprimir();
+    void buscar();
+    void modificar();
+    void eliminar();
+    void Mostrar_Indice();
+    void Orden();
+}firstClass;
+
+class Indice
+{
+    public:
+    char peliculaIndice[100];
+    long int pos;
+
+}indizacion;
+
+void reproduccionList::imprimir()
+{
+    ifstream a("datos.txt");
+    if(!a.good())
+    cout<<"No existe el archivo...";
+    else
+    {
+        while(!a.eof())
+        {
+            a.read((char*)&firstClass,sizeof(firstClass));
+            if(a.eof())
+                break;
+            cout<<"\nID lista:"<<firstClass.pelicula<<endl<<"Nombre de la pelicula: "<<firstClass.nombrePelicula<<endl<<"Categoria: "<<firstClass.categoria<<endl<<"Year: "<<firstClass.anio<<endl;
+        }
+    }
+    a.close();
+}
+
+void reproduccionList::Mostrar_Indice()
+{
+  ifstream a("indice.txt");
+    if(!a.good())
+    cout<<"No existe el archivo";
+    else
+    {
+        while(!a.eof())
+        {
+            a.read((char*)&indizacion,sizeof(indizacion));
+            if(a.eof())
+                break;
+            cout<<"No. Lista de Reproduccion: "<<indizacion.peliculaIndice<<endl<<endl;
+        }
+    }
+    a.close();
+}
+
+
+
+
+void reproduccionList::agregar()
+{
+    fflush(stdin);
+    cout<<"Ingresa ID de LRP (Lista de reproduccion):"<<endl;
+    cin.getline(pelicula,14);
+    cout<<"Nombre de pelicula: "<<endl;
+    cin.getline(nombrePelicula,35);
+    cout<<"Categoria: "<<endl;
+    cin.getline(categoria,40);
+    cout<<"Year: "<<endl;
+    cin.getline(anio,20);
+    fflush(stdin);
+    ofstream a("datos.txt",ios::app);
+    a.write((char*)&firstClass,sizeof(firstClass));
+    indizacion.pos=a.tellp();
+    indizacion.pos-=sizeof(firstClass);
+    aclose();.
+    strcpy(indizacion.peliculaIndice,pelicula);
+    ofstream b("indice.txt",ios::app);
+    b.write((char*)&indizacion,sizeof(indizacion));
+    b.close();
+    orden++;
+    cout<<"=======================================================\n";
+    cout<<"SE AGREGO A LA LISTA DE REPDORUCCION CORRECTAMENTE!!\n";
+    cout<<"=======================================================\n";
+}
+
+void reproduccionList::buscar()
+{
+    char peliculabus[80];
+    int band=0;
+
+    ifstream b("indice.txt");
+    if(!b.good()){
+    cout<<"No existe el archivo";}
+    else{
+    cout<<"\nIngresa ID: "<<endl;
+    fflush(stdin);cin.getline(peliculabus, 100);
+    while(!b.eof())
+    {
+        b.read((char*)&indizacion,sizeof(indizacion));
+        if(b.eof())
+        {
+            break;
+        }
+        if(strcmp(indizacion.peliculaIndice,peliculabus)==0)
+        {
+            ifstream a("datos.txt");
+            a.seekg(indizacion.pos,ios::beg);
+            a.read((char*)&firstClass,sizeof(firstClass));
+            cout<<"No. Lista de Reproduccion: "<<firstClass.pelicula<<endl<<"Nombre de la pelicula: "<<firstClass.nombrePelicula<<endl<<"Categoria: "<<firstClass.categoria<<endl<<"Year:"<<firstClass.anio<<endl;
+            a.close();
+            band=1;
+            break;
+        }
+    }
+    if(band==0)
+    {
+     cout<<"\n NO EXISTE REGISTRO....."   ;
+    }
+    }
+    b.close();
+
+}
+
+void reproduccionList::modificar()
+{
+     char peliculamod[15];
+     int band=0;
+     int opc;
+     ifstream b("indice.txt");
+     if(!b.good()){
+     cout<<"No existe el archivo";}
+     else{
+     cout<<"\nIngresa ID a modificar: "<<endl;
+     fflush(stdin);cin.getline(peliculamod,14);
+     while(!b.eof())
+    {   opc=0;
+        b.read((char*)&indizacion,sizeof(indizacion));
+        if(b.eof())
+        {
+            break;
+        }
+        if(strcmp(indizacion.peliculaIndice,peliculamod)==0 && band==0)
+        {
+            ifstream a("datos.txt");
+            a.seekg(indizacion.pos,ios::beg);
+
+             a.read((char*)&firstClass,sizeof(firstClass));
+             if(a.eof())   break;
+            cout<<"No. Lista de Reproduccion: "<<firstClass.pelicula<<endl<<"Nombre: "<<firstClass.nombrePelicula<<endl<<"Categoria: "<<firstClass.categoria<<endl<<"Year:"<<firstClass.anio<<endl;
+            cout<<"Deseas modificarlo? "<<"1. [Si]  2.[No] \n"; cin>>opc;
+            band=1;
+            a.close();
+            break;
+         }
+         }
+         b.close();
+         if(opc==1)
+{
+    fflush(stdin);
+    ifstream indice("indice.txt");
+    ifstream datos("datos.txt");
+          while(!indice.eof())
+         {
+          indice.read((char*)&indizacion,sizeof(indizacion));
+          cout<<"\n ID:"<<indizacion.peliculaIndice<<"\n";
+          datos.read((char*)&firstClass,sizeof(firstClass));
+           if(indice.eof())
+        {
+            break;
+        }
+        if(strcmp(indizacion.peliculaIndice,peliculamod)==0)
+        {
+        cout<<"No. Lista de Reproduccion: "<<endl;
+       cin.getline(pelicula,14);
+        cout<<"Nombre de la pelicula"<<endl;
+    cin.getline(nombrePelicula,35);
+    cout<<"Categoria de la pelicula: "<<endl;
+    cin.getline(categoria,40);
+    cout<<"Anio de la pelicula: "<<endl;
+    cin.getline(anio,20);
+    fflush(stdin);
+
+     }
+
+          ofstream c("temp.txt",ios::app);
+          c.write((char*)&firstClass,sizeof(firstClass));
+
+        strcpy(indizacion.peliculaIndice,pelicula);
+        ofstream d("tempind.txt",ios::app);
+        d.write((char*)&indizacion,sizeof(indizacion));
+        d.close();
+        c.close();
+
+        }
+
+
+
+}
+
+    if(band==0)
+    {cout<<"\n NO EXISTE REGISTRO....."   ;}
+
+    }
+
+    remove("indice.txt"); rename("tempind.txt","indice.txt");
+    remove("datos.txt");  rename("temp.txt","datos.txt");
+
+
+}
+
+void reproduccionList::eliminar()
+{
+ char peliculaeli[15];
+     int band=0;
+     int opc,cont=0,cont2=0;
+     ifstream b("indice.txt");
+     if(!b.good()){
+     cout<<"No existe el archivo";}
+     else{
+     cout<<"\nIngresa ID a Eliminar: "<<endl;
+     fflush(stdin);cin.getline(peliculaeli,14);
+     while(!b.eof())
+    {
+        b.read((char*)&indizacion,sizeof(indizacion));
+        cont++;
+        if(b.eof())
+        {
+            break;
+        }
+        if(strcmp(indizacion.peliculaIndice,peliculaeli)==0)
+        {
+            ifstream a("datos.txt");
+            a.seekg(indizacion.pos,ios::beg);
+            a.read((char*)&firstClass,sizeof(firstClass));
+             if(a.eof())   break;
+
+            cout<<"No. Lista de Reproduccion: "<<firstClass.pelicula<<endl<<"Nombre: "<<firstClass.nombrePelicula<<endl<<"Categoria: "<<firstClass.categoria<<endl<<"Year:"<<firstClass.anio<<endl;
+            cout<<"Deseas modificarlo? "<<"1. [Si]  2.[No] \n"; cin>>opc;
+            band=1;
+           a.close();
+             break;
+         }
+
+         }
+        b.close();
+
+
+         if(opc==1)
+         {
+          ifstream indice("indice.txt");
+          ifstream datos("datos.txt");
+          while(!indice.eof())
+         {cout<<"entro al ciclo\n";
+          indice.read((char*)&indizacion,sizeof(indizacion));
+          cout<<"\n ID:"<<indizacion.peliculaIndice<<"\n";
+          datos.read((char*)&firstClass,sizeof(firstClass));
+           if(indice.eof())
+        {
+            break;
+        }
+        if(strcmp(indizacion.peliculaIndice,peliculaeli))
+        {cont2++;
+
+
+          cout<<"Nombre de pelicula a quitar de la lista: "<<firstClass.nombrePelicula;
+          ofstream c("temp.txt",ios::app);
+          c.write((char*)&firstClass,sizeof(firstClass));
+
+        strcpy(indizacion.peliculaIndice,pelicula);
+        ofstream d("tempind.txt",ios::app);
+        d.write((char*)&indizacion,sizeof(indizacion));
+        d.close();
+        c.close();
+        }
+        }//while archivo
+       indice.close();
+       datos.close();
+       remove("indice.txt"); rename("tempind.txt","indice.txt");
+    remove("datos.txt");  rename("temp.txt","datos.txt");
+        }//if
+        else
+        {
+
+        }
+
+    if(band==0)
+    {cout<<"\n NO EXISTE REGISTRO....."   ;}
+
+    }
+
+
+}
+
+int main()
+{
+    int opc;
+    do{
+        system("cls");
+        HANDLE hConsole = GetStdHandle( STD_OUTPUT_HANDLE );
+        SetConsoleTextAttribute(hConsole, 15);
+        cout<<"\n==================================================================\n";
+        SetConsoleTextAttribute(hConsole, 3);
+        cout<<"                 --- PELICULAS, SERIES, NOVELAS Y + ---\n\n";
+        SetConsoleTextAttribute(hConsole, 12);
+        cout<<"                               A";
+        SetConsoleTextAttribute(hConsole, 9);
+        cout<<"\n                                L";
+        SetConsoleTextAttribute(hConsole, 6);
+        cout<<"\n                                 V";
+        SetConsoleTextAttribute(hConsole, 14);
+        cout<<"\n                                  E";
+        SetConsoleTextAttribute(hConsole, 10);
+        cout<<"\n                                   L";
+        SetConsoleTextAttribute(hConsole, 13);
+        cout<<"\n                                    +";
+        SetConsoleTextAttribute(hConsole, 3);
+        cout<<"\n\n                 BIENVENIDO AL SERVICIO DE STREAMING!";
+        SetConsoleTextAttribute(hConsole, 15);
+        cout<<"\n==================================================================\n";
+        SetConsoleTextAttribute(hConsole, 7);
+        cout<<"\n\t\t\tMENU PRINCIPAL "<<endl<<endl;
+        SetConsoleTextAttribute(hConsole, 3);
+
+        /*----------------------------------------------------------------------------------------*/
+        cout<<"1) Agregar\n";
+        cout<<"2) Imprimir\n";
+        cout<<"3) Buscar\n";
+        cout<<"4) Modificar\n";
+        cout<<"5) Eliminar\n";
+        cout<<"6) Salir\n";
+        SetConsoleTextAttribute(hConsole, 7);
+        cout<<"\nOPC: ";
+        cin>>opc;
+        switch(opc)
+    {
+
+        case 1:
+        firstClass.agregar();
+
+        break;
+
+        case 2:
+        firstClass.imprimir();
+
+        break;
+
+        case 3:
+        firstClass.buscar();
+        break;
+
+        case 4:
+        firstClass.modificar();
+        break;
+
+        case 5:
+        firstClass.eliminar();
+        break;
+
+        case 6:
+        cout<<"SALIENDO..."<<endl<<endl;
+        return 0;
+        break;
+    }
+    getch();
+    }while(opc!=7);
+    return 0;
+}
